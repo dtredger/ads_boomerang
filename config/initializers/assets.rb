a6b7bd192a6ddda9b69bcea646d99da6b8c33259
Dylan Tredger <d.tredger@gmail.com>
2016-09-02 09:46:51 -0700

    new homepage with signin/ register modals

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( pages/home.js pages/home.scss pages/* stylesheets/pages/*
+javascripts/pages/* )
