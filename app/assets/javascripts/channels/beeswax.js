5c9ec2f63ddbf6a17aef3f8903ab939565f27fae
Dylan Tredger <d.tredger@gmail.com>
2016-09-07 10:10:44 -0700

    campaign creation forms

--- app/assets/javascripts/channels/beeswax.js
+++ app/assets/javascripts/channels/beeswax.js
@@ -1,14 +1,14 @@
 App.beeswax = App.cable.subscriptions.create("BeeswaxChannel", {
   connected: function() {
-    $('#events').prepend("<h2>CONNECT</h2>")
+    $('#events').prepend("<h4>CONNECT</h4>")
   },
 
   disconnected: function() {
-    $('#events').prepend("<h2>DISCONNECT</h2>")
+    $('#events').prepend("<h4>DISCONNECT</h4>")
   },
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    $('#events').prepend("<h2>"+ data.message + "</h2>")
+    $('#events').prepend("<h4>"+ data.message + "</h4>")
   }
 });
