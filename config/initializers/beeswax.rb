399755466d935dbe239cd111936f0306e6953fdd
Dylan Tredger <d.tredger@gmail.com>
2016-10-05 15:00:14 -0700

    to_sym fails on nil

--- config/initializers/beeswax.rb
+++ config/initializers/beeswax.rb
@@ -1,5 +1,5 @@
 require 'beeswax/base'
 
-Beeswax.environment  = ENV["BEESWAX_ENVIRONMENT"].to_sym
+Beeswax.environment  = ENV.fetch("BEESWAX_ENVIRONMENT") { "sandbox" }.to_sym
 Beeswax.api_user     = ENV["BEESWAX_API_USER"]
 Beeswax.api_password = ENV["BEESWAX_API_PASSWORD"]
