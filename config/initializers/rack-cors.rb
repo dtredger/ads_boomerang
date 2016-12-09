33c7299c202a23705487676c9e95ed563fc2ea01
Dylan Tredger <d.tredger@gmail.com>
2016-12-08 16:01:16 -0800

    can't use ENV

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -3,13 +3,14 @@
 if defined? Rack::Cors
 	Rails.configuration.middleware.insert_before 0, Rack::Cors do
 		allow do
-			origins %W(
-								https://#{ENV[CDN_DOMAIN]}
-								http://#{ENV[CDN_DOMAIN]}
-								https://#{ENV[HOST]}
-								http://#{ENV[HOST]}
-								https://ads-boomerang-staging.herokuapp.com
-								http://ads-boomerang-staging.herokuapp.com )
+			origins [
+                "https://d1idnqjey0zziz.cloudfront.net",
+								"http://d1idnqjey0zziz.cloudfront.net",
+                "https://adsboomerang.com",
+                "http://adsboomerang.com",
+                "https://ads-boomerang-staging.herokuapp.com",
+                "http://ads-boomerang-staging.herokuapp.com"
+            ]
 			resource '/assets/*'
 		end
 	end
