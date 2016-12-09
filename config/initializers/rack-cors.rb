622918deb6da36d8e113b453a446066c02a26dd3
Dylan Tredger <d.tredger@gmail.com>
2016-12-08 16:18:07 -0800

    fix cdn url

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -4,8 +4,8 @@ if defined? Rack::Cors
 	Rails.configuration.middleware.insert_before 0, Rack::Cors do
 		allow do
 			origins [
-                "https://d1idnqjey0zziz.cloudfront.net",
-								"http://d1idnqjey0zziz.cloudfront.net",
+                "https://d305q2469s7bp2.cloudfront.net",
+								"http://d305q2469s7bp2.cloudfront.net",
                 "https://adsboomerang.com",
                 "http://adsboomerang.com",
                 "https://ads-boomerang-staging.herokuapp.com",
