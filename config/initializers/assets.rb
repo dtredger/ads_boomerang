7945a7a319e8be220c657912b353b66b45a6659c
Dylan Tredger <d.tredger@gmail.com>
2016-12-13 00:39:09 -0800

    move all asset precompilation to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,9 +5,27 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( base.js pages/home.js pages/home.scss pages/* stylesheets/pages/* javascripts/pages/* stylesheets/* javascripts/* faq/* pricing/* mailers/* bootstrap-notify.js rollbar.js pages/partials/drop_template.css pages/base
-.css images/chrome_top.png)
+
+Rails.application.config.assets.precompile += %w[
+	base.js
+	stylesheets/*
+	javascripts/*
+	stylesheets/pages/*
+	javascripts/pages/*
+	pages/*
+	pages/partials/drop_template.css
+	pages/base.css
+	faq/*
+	pricing/*
+	mailers/*
+	images/*
+	images/chrome_top.png
+	bootstrap-notify.js
+	rollbar.js
+]
