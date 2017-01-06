5c65ad1c29d172ec8f103388f06a0fc41a40c9ae
Dylan Tredger <d.tredger@gmail.com>
2017-01-05 16:18:41 -0800

    restrict intercom to production env

--- spec/factories/websites.rb
+++ spec/factories/websites.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: websites
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  domain_name      :string
+#  provider         :integer
+#  pages            :json
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  hosting_provider :integer
+#  notes            :string
+#  advertiser_id    :integer
+#
+
 FactoryGirl.define do
   factory :website do
     
--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: websites
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  domain_name      :string
+#  provider         :integer
+#  pages            :json
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  hosting_provider :integer
+#  notes            :string
+#  advertiser_id    :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Website, type: :model do
