13fa18d11f4aaffc70f1a30ddec6c734ea23ea63
Dylan Tredger <d.tredger@gmail.com>
2017-01-05 16:41:02 -0800

    use letsencrypt middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  
+  config.middleware.use Letsencrypt::Middleware
 
   # https://github.com/pixielabs/letsencrypt-rails-heroku
   config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
